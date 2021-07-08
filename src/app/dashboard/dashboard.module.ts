708b61367b4c1a4490e6b2fe9a525c2e15813402
shadowalker <01shadowalker01@gmail.com>
2021-07-08 17:58:21 +0430

    Rename component to article-list

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PostListComponent } from './components/post-list/post-list.component';
 import { ArticleFormComponent } from './components/article-form/article-form.component';
 import { TagListComponent } from './components/tag-list/tag-list.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    PostListComponent,
     ArticleFormComponent,
-    TagListComponent
+    TagListComponent,
+    ArticleListComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    DashboardComponent
   ]
 })
 export class DashboardModule { }
