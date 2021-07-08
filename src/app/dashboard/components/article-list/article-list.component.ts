260203bd3695d12d29ef027a09744b07bc85dfc1
shadowalker <01shadowalker01@gmail.com>
2021-07-08 23:09:48 +0430

    Add input to component & Add table template

--- src/app/dashboard/components/article-list/article-list.component.ts
+++ src/app/dashboard/components/article-list/article-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Article } from "src/app/core/models";
 
 @Component({
-  selector: 'r1c-article-list',
-  templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.scss']
+  selector: "r1c-article-list",
+  templateUrl: "./article-list.component.html",
+  styleUrls: ["./article-list.component.scss"],
 })
 export class ArticleListComponent implements OnInit {
+  @Input() articles: Article[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
