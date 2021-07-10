be48f5834b0d3eeee531199c58c6c1141b60184f
shadowalker <01shadowalker01@gmail.com>
2021-07-10 18:45:23 +0430

    Get articles in article list, add links to sidebar

--- src/app/dashboard/components/article-list/article-list.component.ts
+++ src/app/dashboard/components/article-list/article-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { Article } from "src/app/core/models";
+import { BaseService } from "src/app/core/services";
 
 @Component({
   selector: "r1c-article-list",
@@ -7,9 +10,17 @@ import { Article } from "src/app/core/models";
   styleUrls: ["./article-list.component.scss"],
 })
 export class ArticleListComponent implements OnInit {
-  @Input() articles: Article[] = [];
+  articles$: Observable<Article[]>;
 
-  constructor() {}
+  constructor(private baseService: BaseService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getArticles();
+  }
+
+  getArticles() {
+    this.articles$ = this.baseService
+      .get$("articles")
+      .pipe(map(({ articles }) => articles));
+  }
 }
