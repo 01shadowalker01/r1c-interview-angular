e26586cfd5fee68e7471b322f1e11ee70da0fbc8
shadowalker <01shadowalker01@gmail.com>
2021-07-13 01:12:55 +0430

    Add loading-spinner, Add login route for redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
     loadChildren: () =>
       import("./dashboard/dashboard.module").then(m => m.DashboardModule),
   },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
